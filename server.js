9d6ee34df7545f4e1d6e49e7df886d311bcb85f5
Jordan Smith <Jordan@synthesis.co.za>
2023-07-18 21:15:56 +0200

    Test

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const port = process.env.PORT || 5000;
+
+http
+  .createServer(function (req, res) {
+    let url = req.url;
+
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "Access-Control-Allow-Origin": "*" /* @dev First, read about security */,
+      "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+      "Access-Control-Max-Age": 2592000, // 30 days
+    });
+
+    res.write(`<h1>Hello From API ${url}</h1>`);
+    res.end();
+  })
+  .listen(port, function () {
+    console.log("Server listening");
+  });
